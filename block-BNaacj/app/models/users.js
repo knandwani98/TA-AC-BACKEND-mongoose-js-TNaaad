28c0b4d5083db25e4a4d3d0e2910df43fe2db5b9
Kushal Nandwani <kushalnandwani98@gmail.com>
2022-12-19 03:11:19 +0530

    gg

---
+++ block-BNaacj/app/models/users.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  password: { type: String, min: 5, max: 15 },
+  createdAt: { type: Date, default: Date.now() },
+});
